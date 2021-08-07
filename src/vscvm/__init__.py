efb2baffb9339cd2b1df85781cccfdf5b5bb1140
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-08-07 18:56:58 +0530

    Add version listing

--- src/vscvm/__init__.py
+++ src/vscvm/__init__.py
@@ -1,9 +1,20 @@
 """VSCode version manager."""
+import bs4
+import urllib.request
 
 
 def cli() -> None:
     """CLI interface"""
-    print("VSCode version manager.")
+    with urllib.request.urlopen("https://code.visualstudio.com/updates") as request:
+        html = request.read()
+
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        vscode_version_links = soup.select("#docs-navbar a")
+        for link in vscode_version_links[:5]:
+            url = link.get("href")
+            _, _, version = url.rpartition("/")
+            month = link.text
+            print(f"{version} - {month}")
 
 
 def main() -> None:
